d5108a201e7e1e7b5ceaf02ea84cf9b47e747076
danielpmc <danielpd93@gmail.com>
2019-05-03 18:02:48 +0100

    Renamed host to system

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("```Commands: \n !DBR!stats \n !DBR!help \n !DBR!reload \n !DBR!uptime \n !DBR!suggest \n !DBR!ping \n !DBR!reload \n !DBR!nextsong \n !DBR!forceleave```");
+    message.channel.send("```Commands: \n !DBR!stats \n !DBR!help \n !DBR!reload \n !DBR!uptime \n !DBR!suggest \n !DBR!ping \n !DBR!reload \n !DBR!nextsong \n !DBR!forceleave \n !DBR!system```");
 };
